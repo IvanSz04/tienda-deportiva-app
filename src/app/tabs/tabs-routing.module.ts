0ed97ead856c19093b350a9bce5ec6a22052fd5a
Ivan Solorzano <solorzanoivan608@gmail.com>
2025-09-26 22:55:27 -0500

    Utilizamos Lazy Loading para el sistema de routing principal, estableciendo de una vez para nuestro login y registrer, creamos carpetas organizadas y establecemos la estructura para de nuestro nav como Inicio, Productos, Carrito y Perfil

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../pages/home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'products',
+        loadChildren: () => import('../pages/products/products.module').then(m => m.ProductsPageModule)
+      },
+      {
+        path: 'cart',
+        loadChildren: () => import('../pages/cart/cart.module').then(m => m.CartPageModule)
+      },
+      {
+        path: 'profile',
+        loadChildren: () => import('../pages/profile/profile.module').then(m => m.ProfilePageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
